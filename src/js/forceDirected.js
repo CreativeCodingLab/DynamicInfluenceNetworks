3baf54cc8a8b26c167db3739c7aa1ae5752e806a
Kristine Lee <hmsklee@users.noreply.github.com>
2016-11-23 07:30:29 -0600

    gradient thoughts...

--- src/js/forceDirected.js
+++ src/js/forceDirected.js
@@ -16,6 +16,27 @@ function createForceDirectedGraph() {
     .style("padding", "20px")
     .style("fill", "#eee");
 
+  // define color gradients
+  var defs = svg.append('defs');
+  var red1 = defs.append('linearGradient')
+      .attr('id','redLine')
+      .attr('x1',0)
+      .attr('y1',0)
+      .attr('x2',1)
+      .attr('y2',0)
+  red1.append('stop')
+      .attr('offset','0%')
+      .attr('stop-color','yellow');
+  red1.append('stop')
+      .attr('offset','100%')
+      .attr('stop-color','red');
+
+  defs.append('linearGradient')
+      .attr('id','redReverse')
+      .attr('xlink:href','#redLine')
+      .attr('x1',1)
+      .attr('x2',0)
+
 
   var linkGroup = svg.append("g")
     .attr("class", "linkGroup");
@@ -112,6 +133,7 @@ function createForceDirectedGraph() {
       .range([5, 10]);
 
     for (var key in filteredData) {
+      filteredData[key].radius = radiusScale(filteredData[key].hits);
       filteredData[key].x = width / 2;
       filteredData[key].y = height / 2; 
     }
@@ -126,9 +148,7 @@ function createForceDirectedGraph() {
       .attr("transform", (d, i) => {
         return "translate(" + d.x + ", " + d.y + ")";
       })
-      .attr("r", (d) => {
-        return radiusScale(d.hits);
-      })
+      .attr("r", d => d.radius)
       .style("fill", "#abd9e9")
       .style("stroke", "#2c7bb6")
       .style("stroke-width", 1)
@@ -182,16 +202,23 @@ function createForceDirectedGraph() {
       .data(App.panels.forceDirected.links)
     .enter().append("g")
       .attr('class', 'linkElement')
-      .attr('fill','none');
+      .attr('fill','none')
+      .style("stroke", (d) => {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y;
+        if (d.value > 0) {
+          return "#33a02c";
+        }
+        else {
+          return dx >= 0 ? "url(#redLine)" : "url(#redReverse)";
+        }
+        // return d.value > 0 ? "#33a02c" : "#e31a1c"
+      });
 
     // main line
     linkGroupElement.append('path')
       .attr("class", "link")
-      .style("stroke", (d) => {
-        var color = d3.hsl(d.value > 0 ? "#33a02c" : "#e31a1c");
-        color.opacity = (strokeScale(Math.abs(d.value))-0.3)*0.5/0.7+0.5;
-        return color.toString();
-      })
+      .style('stroke-opacity', (d) => (strokeScale(Math.abs(d.value))-0.3)*0.5/0.7+0.5)
       .style("stroke-width", (d) => {
         return strokeScale(Math.abs(d.value));
       });
@@ -199,17 +226,17 @@ function createForceDirectedGraph() {
     // invisible line for collisions
     linkGroupElement.append('path')
       .attr("class", "link")
-      .style("stroke", 'rgba(0,0,0,0)')
+      .style("stroke-opacity", 0)
       .style("stroke-width", 8)
       .on("mouseover", (d, i) => {
-        d3.select(event.target)
-          .style('stroke', d.value > 0 ? // "#33a02c" : "#e31a1c"
-                "rgba(51,160,44,0.5)" : "rgba(227,26,28,0.5)"
-            );
         var dx = d.target.x - d.source.x,
             dy = d.target.y - d.source.y;
         var ex = event.x - d.target.x + 20,
             ey = event.y - d.target.y;
+
+
+        d3.select(event.target)
+          .style('stroke-opacity',0.5);
 
         if (event.x < width-300) {
           link_tip
@@ -230,7 +257,7 @@ function createForceDirectedGraph() {
       .on("mouseout", (d, i) => {
         d3.select(event.target)
           .transition()
-          .style('stroke','rgba(0,0,0,0)');
+          .style('stroke-opacity',0);
         link_tip.hide(d,i);
       });
   }
@@ -296,15 +323,24 @@ function createForceDirectedGraph() {
 
             if (dr == 0) { return ""; }
 
-            var nx = -20* dx / dr,
-                ny = -20*dy / dr;
+            var nx = -dx / dr,
+                ny = -dy / dr;
+
+            if (dr < 20) { dr /= 2; }
+
+            var t = {
+              x: target.x + (target.radius+8)*nx, 
+              y: target.y + (target.radius+8)*ny
+            };
+
+            nx *= 10, ny *= 10;
 
             // normal ellipse version...
             return  "M" + source.x + "," + source.y + 
                     "A" + dr + "," + dr + " 0 0,1 " + 
-                    (target.x+nx) + "," + (target.y+ny)+
-                    // "m" + (nx-ny/2) + ',' + (ny+nx/2) + 
-                    // "L" + (target.x+nx) + "," + (target.y+ny) +
+                    t.x + "," + t.y+
+                    "m" + (nx-ny/2) + ',' + (ny+nx/2) + 
+                    "L" + t.x + "," + t.y+
                     "l" + (nx+ny/2) + ',' + (ny-nx/2);
           });
       });
